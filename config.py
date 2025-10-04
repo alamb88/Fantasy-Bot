32583dd83748916e1a064aff794ac2964521c8f4
alamb88 <aidanpatricklambert@gmail.com>
2025-10-05 04:52:41 +1100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+CREATOR_WHITELIST = {
+    "josh_lloyd": {
+        "aliases": ["Locked On Fantasy Basketball", "Josh Lloyd", "Locked On Fantasy"],
+        "weight": 1.0
+    }
+}
+
+LEAGUE_SETTINGS = {
+    "scoring": "9-cat",
+    "categories": ["PTS","REB","AST","STL","BLK","3PM","FG%","FT%","TOV"],
+    "risk_tolerance": 0.35,
+    "punt_flex": 0.25
+}
